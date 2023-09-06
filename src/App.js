ad25e8d74c98fabe2806fd42de50301bc5cf918e
박준서 <jswing5267@naver.com>
2023-09-07 00:39:19 +0900

    dark, light mode

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 89px;
-`;
-const Box = styled.div`
-  background-color: tomato;
-  height: 200px;
-  width: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 70px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😁</Emoji>
-      </Box>
-      <Emoji>🤣</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
